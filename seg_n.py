b2f39d7e5623e5a2a236e67d00ced30c2970cb64
Mofii <mofii.tech@gmail.com>
2019-06-23 21:09:19 -0400

    update

--- seg_n.py
+++ seg_n.py
@@ -19,7 +19,7 @@ from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.layers.merge import concatenate
 from keras.optimizers import Adam
 
-def new_residual_block(X, filters, channels, stage):
+def residual_block(X, filters, channels, stage):
 
     '''Residual block
 
@@ -61,48 +61,47 @@ def cell_detector(input_shape=(100, 100, 3)):
     
     input_layer = Input(input_shape)
 
-    print(type(new_residual_block))
     X = Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding='same')(input_layer)
-    res_out_1 = new_residual_block(X, filters=[32, 32], channels=32, stage=1)(X)
+    res_out_1 = residual_block(X, filters=[32, 32], channels=32, stage=1)
     X = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same')(res_out_1)
 
     # DownSampling block 1
     X = AveragePooling2D(pool_size=(2, 2), padding='valid')
-    res_out_2 = residual_block(X, [64, 64], 64, 2)(X)
+    res_out_2 = residual_block(X, [64, 64], 64, 2)
     X = Conv2D(128, kernel_size=(3, 3), strides=(1, 1), padding='same')(res_out_2)
 
     # DownSampling block 2
     X = AveragePooling2D(pool_size=(2, 2), padding='valid')
-    res_out_3 = residual_block(X, [128, 128], 128, 3)(X)
+    res_out_3 = residual_block(X, [128, 128], 128, 3)
     X = Conv2D(256, kernel_size=(3, 3), strides=(1, 1), padding='same')(res_out_3)
 
     # DownSampling block 3
     X = AveragePooling2D(pool_size=(2, 2), padding='valid')
-    res_out_4 = residual_block(X, [256, 256], 256, 4)(X)
+    res_out_4 = residual_block(X, [256, 256], 256, 4)
     
     # DownSampling block 4
     X = AveragePooling2D(pool_size=(2, 2), padding='valid')
-    X = residual_block(X, [256, 256], 256, 5)(X)
+    X = residual_block(X, [256, 256], 256, 5)
 
     # UpSampling block 1
     X = UpSampling2D(size=(2, 2), interpolation='bilinear')
     X = concatenate([res_out_4, X])
-    X = residual_block(X, [256, 256], 256, 5)(X)
+    X = residual_block(X, [256, 256], 256, 5)
 
     # UpSampling block 2
     X = UpSampling2D(size=(2, 2), interpolation='bilinar')
     X = concatenate([res_out_3, X])
-    X = residual_block(X, [128, 128], 128, 6)(X)
+    X = residual_block(X, [128, 128], 128, 6)
 
     # UpSampling block 3
     X = UpSampling2D(size=(2, 2), interpolation='bilinear')
     X = concatenate([res_out_2, X])
-    X = residual_block(X, [64, 64], 64, 7)(X)
+    X = residual_block(X, [64, 64], 64, 7)
 
     # UpSampling block 4
     X = UpSampling2D(size=(2, 2), interpolation='bilinear')
     X = concatenate([res_out_1, X])
-    X = residual_block(X, [32, 32], 32, 8)(X)
+    X = residual_block(X, [32, 32], 32, 8)
 
     # Conv to ouput map
     X = Conv2D(filters=1, kernel_size=(3, 3), strides=(1, 1), padding='same')(X)
@@ -124,7 +123,7 @@ if __name__=='__main__':
         np.random.seed(123)
         A_prev = tf.placeholder('float', [5, 100, 100, 3])
         X = np.random.randn(5, 100, 100, 3)
-        A = new_residual_block(A_prev, filters=[3, 3], channels=3, stage=1)
+        A = residual_block(A_prev, filters=[3, 3], channels=3, stage=1)
         test.run(tf.global_variables_initializer())
         out = test.run([A], feed_dict={A_prev: X, K.learning_phase(): 0})
         print('out = ' + str(out[0][1][1][0]))
